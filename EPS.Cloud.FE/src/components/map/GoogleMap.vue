99556dc4f365898c5dba6233332e380d566bf736
NguyenTrongTue <20020498@vnu.edu.vn>
2024-05-01 18:31:05 +0700

    commit code

--- EPS.Cloud.FE/src/components/map/GoogleMap.vue
+++ EPS.Cloud.FE/src/components/map/GoogleMap.vue
@@ -18,7 +18,7 @@ export default {
   data() {
     return {
       loader: new Loader({
-        apiKey: 'AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg',
+        apiKey: 'AIzaSyBRAmtI_jL74qMIFWazRRAApR1qdXCB16A',
         // 'AIzaSyAQ2Jcz1gngtqWN1w-mtQ2ja1i49EerB50', //"AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg",
         version: "weekly",
         libraries: ["places"],
